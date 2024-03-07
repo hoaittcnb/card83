44d3034fec38439652fd3492005a0ab129d116e6
hoaittcnb <162543365+hoaittcnb@users.noreply.github.com>
2024-03-07 13:04:37 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Đào';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Gửi đến Đào';
+const contentCard = 'Chúc bạn nhận được nhiều quà, nhiều hoa, nhiều lời khen, lời chúc của phái nam trong ngày hôm nay. Sớm có người yêu nha bạn vì bạn thực sự rất xinh đẹp đó.';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
